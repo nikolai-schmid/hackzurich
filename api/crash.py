adbab83a347614ca2fd2316821a54ba0c510b1e8
Ali Ariff <ali.ariff12@gmail.com>
2018-09-15 13:29:33 +0200

    randomize result

--- api/crash.py
+++ api/crash.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import random
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def main():
+    return "It's Worked"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     content = request.get_json()
     routes = content['routes']  # [[1.2, 4.5], [1.2, 4.5], [1.2, 4.5]]
     persona = content['persona']  # { "age": 17, weather: 1 }
-    return "Test"
+    return jsonify([random.randrange(0, 2) for x in range(len(routes))])
